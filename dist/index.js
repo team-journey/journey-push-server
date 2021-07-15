37b4651d5deb02bd6c9e3204ec92556d0f487725
Kim HyunJi <khyunjiee@gmail.com>
2021-07-16 03:02:15 +0900

    feat: 시간 변경

--- dist/index.js
+++ dist/index.js
@@ -18,7 +18,7 @@ const collapse_key = '';
 // admin.initializeApp({
 //   credential: admin.credential.cert(firebaseAccount),
 // });
-const journeyAlarm = node_schedule_1.default.scheduleJob('0 32 02 * * *', async function () {
+const journeyAlarm = node_schedule_1.default.scheduleJob('0 10 03 * * *', async function () {
     let date = new Date();
     console.log(`시작 시각 ${dayjs_1.default(date.toLocaleString('en', { timeZone: 'Asia/Seoul' })).format('YYYY-MM-DD hh:mm:ss')} 입니다.`);
     // 전체 유저의 fcm token 값들을 가져온다.
@@ -26,16 +26,14 @@ const journeyAlarm = node_schedule_1.default.scheduleJob('0 32 02 * * *', async 
     // const registration_ids = await axios.get(`${config.journeyURL}/api/users`);
     const registration_ids = [
         'fZBZtMtjk0pdqiyW-Qln7-:APA91bHFcZinM88tH7GJuMFCDdhKG4Nzd4m0uQNJjvXGQ-d4B6PAbO5X72VxzsEFjcoab8EEgN3CYlV82H58StIjfPdSvFhbO1xYRsnSGB7HHw-Bj4zb44NTxD6tT6b-bx1lKhSLOwG4',
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7fSwiaWF0IjoxNjI2MzMyMzkyfQ.lzqwiG-7w6PPQtw-arpjmrc6qEGOO3vi6UoWCtUP6oo',
         'e_0GyXigjUp_nEwlqJXf4y:APA91bGBRb9ID8GiBpyf495pg91MGU1vGKKbtLhPxwXXeIAPGh6fbLI0Td1YVLd-9tzBn__ruqGBkG-Kzfqyq9DoiPsgRcDB7JV0ju3Ad1O-02atOCcsHAim-7yt-acsB1Fek5I5DZ1H',
         'ecVV7Tk4DEXvra9nJyre19:APA91bEyokK8ly52zRJe9u5w9ma8Zhtc9uNO-L92ZKgvq2EUBnq9USCjo1r36TXCU8RppgfFbPsB2xoKghMnkThYyRzN1mmv_lYsH5y94yGKK_mGmJKOJRMTvBswN_EEnqVml2m88FZL',
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7fSwiaWF0IjoxNjI2Mjk3Mjg2fQ.j94Z3ujBSsu4fxyFh7wlOeFMLqmEWfWVI9LQB59fK2k'
     ];
     console.log(registration_ids);
     const message = {
         registration_ids: registration_ids,
         notification: {
-            title: '잘잤어 우리 아기 고양이?',
+            title: '작업하고있니 우리 아기 고양이?',
         },
     };
     fcm.send(message, function (err, response) {
@@ -50,30 +48,4 @@ const journeyAlarm = node_schedule_1.default.scheduleJob('0 32 02 * * *', async 
     });
     console.log(`종료 시각 ${dayjs_1.default(date.toLocaleString('en', { timeZone: 'Asia/Seoul' })).format('YYYY-MM-DD hh:mm:ss')} 입니다.`);
 });
-// app.use(express.json());
-// // error handler
-// app.use(function (err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get("env") === "production" ? err : {};
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.json({
-//     message: err.message,
-//     error: err
-//   });
-//   res.render("error");
-// });
-// app
-//   .listen(5000, () => {
-//     console.log(`
-//     ################################################
-//     🛡️  Server listening on port: 5000 🛡️
-//     ################################################
-//   `);
-//   })
-//   .on("error", (err) => {
-//     console.error(err);
-//     process.exit(1);
-//   });
 //# sourceMappingURL=index.js.map
