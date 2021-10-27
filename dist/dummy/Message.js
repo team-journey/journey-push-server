c71ca770d3925c4d28fca105dfd26592cabf31c0
Ubuntu <ubuntu@ip-172-31-4-246.ap-northeast-2.compute.internal>
2021-10-27 18:03:59 +0000

    chore: rebuild

---
+++ dist/dummy/Message.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.notSuccessMent = exports.eveningMent = exports.morningMent = void 0;
+exports.morningMent = [
+    ["ㅁㅁㅁ, 모행?", "오늘도 나랑 놀아주려 왔구나!!!", "씐나게 놀아보자~!"],
+    ["ㅁㅁㅁ~ 기다렸다구~", "재밌는 오늘의 챌린지를 하면서", "나랑 놀자~~"],
+    ["야아아악!!! ㅁㅁㅁ! 나! 랑! 놀! 아! 나랑! 놀아! 줘!!!"]
+];
+exports.eveningMent = [
+    ["벌써 밤이야! 인증하고 일찍 자자-내일 만나 안뇽"],
+    ["ㅁㅁㅁ, 바쁜 하루 중에도", "자신을 위한 휴식은 잊지마!!! =ㅇ="],
+    ["ㅁㅁㅁ 밥먹었냐? 뭐먹었냐?", "맛있었냠? 저녁은 뭐먹을거냠?"]
+];
+exports.notSuccessMent = [
+    ["ㅁㅁㅁ, 오늘 챌린지도 잊으면 안돼~~!!! ‘ㅅ'"],
+    ["어이 ㅁㅁㅁ, 동작그만!", "지금 뭐허냐? 다른 일 일시 중지!!"],
+    ["ㅁㅁㅁ... 날 잊었던 거야?", "아니면 나보다 더 재밌는 친구랑 있었던 거야?", "조금 서운해!"],
+    ["ㅁㅁㅁ!", "왜 요즘 연락이 뜸해 =ㅅ=", "어떻게 사는지 말해주러 와라"]
+];
+//# sourceMappingURL=Message.js.map
