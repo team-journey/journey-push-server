7f2d86d59983c113cdcb9ec3f3b0b050068c0b07
김현지 <khyunjiee@gmail.com>
2021-11-11 12:29:46 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -81,7 +81,7 @@ const evening = schedule.scheduleJob('0 0 11 * * *', async function () {
   );
 });
 
-const init = schedule.scheduleJob('0 0 20 * * *', async function () {
+const init = schedule.scheduleJob('0 31 21 * * *', async function () {
   let date = new Date();
   console.log(
     `시작 시각 ${dayjs(date.toLocaleString('en', { timeZone: 'Asia/Seoul' })).format(
